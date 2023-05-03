99ed5add9914debc9f56c3f2b85aeb7d31d212d5
LiamWindmolders <131238341+LiamWindmolders@users.noreply.github.com>
2023-05-03 07:50:39 +0000

    FIXED LINK

---
+++ webserver/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+const port = 3000
+
+app.get('', (req, res) => {
+    res.sendFile(path.join(__dirname, '/portfolio/index.html'));
+})
+
+app.get('project1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/project1/index.html'));
+})
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+    console.log(`This app listening on port ${port}`)
+})
