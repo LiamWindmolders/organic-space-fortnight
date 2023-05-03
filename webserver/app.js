24a55bcd609900becd7678a4f67a93f82d5db693
LiamWindmolders <131238341+LiamWindmolders@users.noreply.github.com>
2023-05-03 07:09:25 +0000

    Pending changes exported from your codespace

---
+++ webserver/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+const port = 3000
+
+app.get('', (req, res) => {
+    res.sendFile(path.join(__dirname, '/portfolio/index.html'));
+})
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+    console.log(`This app listening on port ${port}`)
+})
